5ed071211d312862d53d164f09fdccd86aa2d9ab
Shubham Singh <87699417+detonators@users.noreply.github.com>
2022-07-06 15:58:09 +0530

    Add files via upload

---
+++ background.py
@@ -0,0 +1,13 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, background = cap.read()
+    if ret:
+        cv2.imshow("image", background)
+        if cv2.waitkey(5) == ord('q'):
+            cv2.imwrite("image.jpg", background)
+            break
+cap.release()
+cv2.destroyAllWindows()      
